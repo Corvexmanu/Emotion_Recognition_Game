b541e72b88420127d14352e660cf524e9e1b46fc
BogdanTodor <bogdan@siteoffice.mobi>
2019-09-01 14:34:53 +1000

    predictions included

--- server/app.py
+++ server/app.py
@@ -15,19 +15,25 @@ def loadModel():
     global model
     # model variable refers to the global variable
     model = load_model("./MY_48_Fer_0.67_bal.hdf5")
+    model._make_predict_function()
 
 
-def loadimage(path, verbose=False):
-    image = cv2.imread(path)
-    image = cv2.resize(image, (48, 48))
+def loadimage(image, verbose = False):
+    image = np.array(image.split(','))
+    image = image.astype('float32')
+    image = np.reshape(image,(240,240,4))
+    # cv2.imshow('j',image)
+    # cv2.waitKey()
+    image = image[:, :, 0:3]
+    image =cv2.resize(image,(48,48))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("./Test.jpg", image)
     image = np.expand_dims(image, -1).astype('float64')
-    image -= np.mean(image, axis=0)
-    image /= np.std(image, axis=0)
     X = []
     X.append(image)
     X = np.array(X)
     return X
+
 
 
 def makePrediction(model, image_to_predict):
@@ -60,15 +66,13 @@ def home_endpoint():
 
 @app.route('/predict', methods=['POST'])
 def get_prediction():
-    image_data = request.get_data()  # Get data posted as a json
-
-    # return path
-    # x = loadimage(image_data)
-    # runs globally loaded model on the data
-    # prediction = str(makePrediction(model, x))
-    response = str([3, 4])
-
-    return response
+    # Works only for a single sample
+    if request.method == 'POST':
+        data = request.get_data()  # Get data posted   
+        image = data.decode("utf-8")             
+        x = loadimage(image)
+        prediction = makePrediction(model,x)  # runs globally loaded model on the data
+    return str(prediction)
 
 
 if __name__ == '__main__':
