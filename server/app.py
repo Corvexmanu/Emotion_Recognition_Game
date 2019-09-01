f4bccbf206939f0145e556adc059b1ae073c1789
Atrey Gajjar <gajjar_atrey98@hotmail.com>
2019-09-01 13:00:58 +1000

    sending image data to server

--- server/app.py
+++ server/app.py
@@ -3,20 +3,23 @@
 import numpy as np
 from flask import Flask, request
 from keras.models import load_model
+from flask_cors import CORS
 import cv2
 
 model = None
 app = Flask(__name__)
+cors = CORS(app)
 
 
 def loadModel():
     global model
     # model variable refers to the global variable
-    model = load_model("./XCEPTION_48_Karol_0.66.hdf5")
+    model = load_model("./MY_48_Fer_0.67_bal.hdf5")
 
-def loadimage(path, verbose = False):
+
+def loadimage(path, verbose=False):
     image = cv2.imread(path)
-    image =cv2.resize(image,(48,48))    
+    image = cv2.resize(image, (48, 48))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = np.expand_dims(image, -1).astype('float64')
     image -= np.mean(image, axis=0)
@@ -24,30 +27,31 @@ def loadimage(path, verbose = False):
     X = []
     X.append(image)
     X = np.array(X)
-    return X    
+    return X
 
-def makePrediction(model,image_to_predict):
-  yhat= model.predict(image_to_predict)
-  yh = yhat.tolist()
-  count = 0
-  for value in yh[0]:
-    count = count + 1
-    if count == 1:
-      print("Angry    = " + "\t" + str(round(value*100)) + "%")
-    elif count == 2:
-      print("Disgust  = " + "\t" + str(round(value*100)) + "%")
-    elif count == 3:
-      print("Fear     = " + "\t" + str(round(value*100)) + "%")
-    elif count == 4:
-      print("Happy    = " + "\t" + str(round(value*100)) + "%")
-    elif count == 5:
-      print("Sad      = " + "\t" + str(round(value*100)) + "%")
-    elif count == 6:
-      print("Surprise = " + "\t" + str(round(value*100)) + "%")
-    elif count == 7:
-      print("Neutral  = " + "\t" + str(round(value*100)) + "%")  
-  return yh[0]
-    
+
+def makePrediction(model, image_to_predict):
+    yhat = model.predict(image_to_predict)
+    yh = yhat.tolist()
+    count = 0
+    for value in yh[0]:
+        count = count + 1
+        if count == 1:
+            print("Angry    = " + "\t" + str(round(value*100)) + "%")
+        elif count == 2:
+            print("Disgust  = " + "\t" + str(round(value*100)) + "%")
+        elif count == 3:
+            print("Fear     = " + "\t" + str(round(value*100)) + "%")
+        elif count == 4:
+            print("Happy    = " + "\t" + str(round(value*100)) + "%")
+        elif count == 5:
+            print("Sad      = " + "\t" + str(round(value*100)) + "%")
+        elif count == 6:
+            print("Surprise = " + "\t" + str(round(value*100)) + "%")
+        elif count == 7:
+            print("Neutral  = " + "\t" + str(round(value*100)) + "%")
+    return yh[0]
+
 
 @app.route('/')
 def home_endpoint():
@@ -56,15 +60,15 @@ def home_endpoint():
 
 @app.route('/predict', methods=['POST'])
 def get_prediction():
-    # Works only for a single sample
-    if request.method == 'POST':
-        data = request.get_data()  # Get data posted as a json 
-        # print(data)
-        path = "./" + str(data.decode("utf-8"))
-        # return path
-        x = loadimage(path)
-        prediction = makePrediction(model,x)  # runs globally loaded model on the data
-    return str(prediction)
+    image_data = request.get_data()  # Get data posted as a json
+
+    # return path
+    # x = loadimage(image_data)
+    # runs globally loaded model on the data
+    # prediction = str(makePrediction(model, x))
+    response = str([3, 4])
+
+    return response
 
 
 if __name__ == '__main__':
